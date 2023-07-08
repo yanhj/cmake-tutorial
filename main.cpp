296833c76ff38f332881c6246751e18019788a53
yanhj <y974183789@gmail.com>
2023-07-08 11:04:20 +0800

    [feature]: gitignore

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 
 int main(int argc, char** argv) {
     std::cout << "Hello, World!" << std::endl;
-    std::cout << VersionInfo::APP_VERSION_MAJOR << std::endl;
+    std::cout << VersionInfo::APP_VERSION_MAJOR << "."
+    << VersionInfo::APP_VERSION_MINOR << "."
+    << VersionInfo::APP_VERSION_PATCH
+    << std::endl;
     return 0;
 }
